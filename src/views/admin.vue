d43a6b20456ab882e99a1935e93c8a9d28248ae0
Xu Yihao <yihao_xu@126.com>
2020-09-08 21:10:36 +0800

    用户表

--- src/views/admin.vue
+++ src/views/admin.vue
@@ -1,9 +1,57 @@
 // write by xyh
 <template>
   <div>
-
+    <el-card class="title-card">所有用户</el-card>
+    <el-table :data="userLists"
+              stripe
+              id="users-table">
+      <el-table-column prop="id"
+                       label="ID"
+                       width="40">
+      </el-table-column>
+      <el-table-column prop="username"
+                       label="用户名"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="created_at"
+                       label="创建时间"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="address"
+                       label="地址"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="email"
+                       label="Email"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="phone"
+                       label="电话号码"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="is_renter"
+                       label="是否是租借者"
+                       width="180">
+      </el-table-column>
+    </el-table>
+    <el-card class='title-card'>所有设备</el-card>
   </div>
 </template>
+
+<style scoped>
+.title-card {
+  width: 60%;
+  margin: auto;
+  margin-top: 20px;
+  position: relative;
+}
+#users-table {
+  position: relative;
+  margin: 0 auto;
+  width: 60%;
+  max-height: 600px;
+}
+</style>
 
 <script>
 import Axios from 'axios'
@@ -17,11 +65,10 @@ export default {
     Axios.get('api/v1/users', {})
       .then((response) => {
         this.userLists = response.data
-        alert(this.userLists)
         console.log(this.userLists)
       })
       .catch((error) => {
-        alert(error)
+        alert('error:' + error)
       })
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.0.106:8000',
+        target: 'http://192.168.0.107:8000',
         ws: true,
         changeOrigin: true
       }
