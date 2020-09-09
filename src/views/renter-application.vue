3c19772c2e4b60fca74d1039e9cd94bc04a1ad3e
Xu Yihao <yihao_xu@126.com>
2020-09-09 14:36:29 +0800

    renter and rent card

---
+++ src/views/renter-application.vue
@@ -0,0 +1,20 @@
+// writen by xyh
+<template>
+  <div>
+    <renter-application-card :id="id"></renter-application-card>
+  </div>
+</template>
+
+<script>
+import renterApplicationCard from '../components/renter-application-card'
+export default {
+  components: {
+    'renter-application-card': renterApplicationCard
+  },
+  data: function () {
+    return {
+      id: parseInt(this.$route.params.renterApplicationId)
+    }
+  }
+}
+</script>
