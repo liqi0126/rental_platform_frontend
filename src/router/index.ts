cc4606fe5f84e58f5a5358da5eb3d191708f0332
Husaimawx <mwx18@mails.tsinghua.edu.cn>
2020-09-09 23:13:35 +0800

    with eslint

--- src/router/index.ts
+++ src/router/index.ts
@@ -48,11 +48,6 @@ const routes: Array<RouteConfig> = [
     component: () => import('../views/analysis.vue')
   },
   {
-    path: '/logon',
-    name: 'logon',
-    component: () => import('../views/logon.vue')
-  },
-  {
     path: '/create-equipment',
     name: 'create-equipment',
     component: () => import('../views/create-equipment.vue')
